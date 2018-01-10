c1c4770df2ea27ab631b8aa702965f0aa6b419a6
github username <lina.rudashevski@gmail.com>
2018-01-10 14:26:56 -0500

    added jwt token stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+
+  def issue_token(payload)
+    JWT.encode(payload, ENV['secret'], 'HS256')
+  end
+
+  def current_user
+    @user ||= User.find_by(id: user_id)
+  end
+
+  def user_id
+    decoded_token.first['id']
+  end
+
+  def decoded_token
+    begin
+       JWT.decode(request.headers['Authorization'], ENV['secret'], true, { :algorithm => 'HS256' })
+     rescue JWT::DecodeError
+      [{}]
+     end
+  end
+
+  def authorized
+    render json: {message: "Not welcome" }, status: 401 unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
